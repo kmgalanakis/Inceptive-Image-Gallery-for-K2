af8ebb8554f1c3d3759f6b6d0338ea84fbfd0c93
Konstantinos Galanakis <kmgalanakis@gmail.com>
2013-07-31 12:01:53 +0300

    K2 2.6.6 compatibility fixed after fixing compatibility for 2.6.7

--- plg_k2_incptvk2imagegallery_joomla25-30/incptvk2imagegallery/includes/elements/incptvk2imagegallery.php
+++ plg_k2_incptvk2imagegallery_joomla25-30/incptvk2imagegallery/includes/elements/incptvk2imagegallery.php
@@ -117,4 +117,54 @@ if(version_compare(JVERSION,'3.0.0','l'))
 		}
 
 	}
+	
+	class JElementIncptvK2ImageGallery extends JElement
+	{
+		// Declare the element type
+		var $_name = 'image';
+		
+		// K2 plugin name. Used to namespace parameters.
+		var $pluginName = 'incptvk2imagegallery';
+
+		// This function returns the input of the element
+		function fetchElement($name, $value, &$node, $control_name)
+		{   
+			$document = JFactory::getDocument();
+			
+			// Get the K2 Item id.
+			$id = JRequest::getInt('cid');
+
+			// Get the K2 item table class and load the item
+			$item = JTable::getInstance('K2Item', 'Table');
+			$item->load($id);
+			
+			// Check if we already have videos for this item
+			$k2PluginData = new K2Parameter($item->plugins, '', $this->pluginName);
+			$images = $k2PluginData->get('Images', array()); //print_r($images);
+			$imageTitles = $k2PluginData->get('ImageTitles', array()); //print_r($imageTitles);
+			$imageDescriptions = $k2PluginData->get('ImageDescriptions', array()); //print_r($imageDescriptions);
+			
+			// Filter the videos array to remove any empty entries
+			$images = array_filter($images);
+			$imageTitles = array_filter($imageTitles);
+			$imageDescriptions = array_filter($imageDescriptions);
+			
+			$igParameters = $k2PluginData->get('IGParameters');
+			$k2IGposition = $k2PluginData->get('k2IGposition');
+			$k2IGtheme = $k2PluginData->get('k2IGtheme');
+			
+			// Now we have the variables we need so let's include the HTML form (MVC FTW!)
+			ob_start();
+			include 'form.php';
+			$output = ob_get_clean();
+			return $output;
+		}
+
+		// This function returns the element label. We are returning NULL since we use our own HTML form
+		function fetchTooltip($label, $description, &$xmlElement, $control_name = '', $name = '')
+		{
+			return NULL;
+		}
+
+	}
 }
